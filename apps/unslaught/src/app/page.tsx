30c909cc1fe0deef0e89869cdd8c6fa5310b5275
mhmadamrii <interceptorghost4@gmail.com>
2025-01-25 22:37:47 +0700

    chore:

---
+++ apps/unslaught/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@omnnitrix/ui';
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Hello world</Button>
+    </div>
+  );
+}
---
+++ apps/wkwk/src/app/app.tsx
@@ -0,0 +1,14 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+import { Button } from '@omnnitrix/ui';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <Button>What the heck?</Button>
+    </div>
+  );
+}
+
+export default App;
