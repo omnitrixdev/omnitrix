3c2f9096814e785416538d2f0c66dbf1c03adf24
mhmadamrii <interceptorghost4@gmail.com>
2025-01-26 22:46:38 +0700

    chore: something

---
+++ trpc/src/lib/trpc.tsx
@@ -0,0 +1,11 @@
+import styles from './trpc.module.css';
+
+export function Trpc() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Trpc!</h1>
+    </div>
+  );
+}
+
+export default Trpc;
